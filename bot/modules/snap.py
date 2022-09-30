2dec09d284663597886da3704916b24c8a7adaaf
bvgd41 <114703422+bvgd41@users.noreply.github.com>
2022-09-30 14:18:25 +0300

    Update snap.py

--- bot/modules/snap.py
+++ bot/modules/snap.py
@@ -38,11 +38,11 @@ async def reply_shortens(bot, update):
     try:
         for i in data["props"]["pageProps"]["story"]["snapList"]:
             await update.reply_document(
-                document=i["snapUrls"]["mediaUrl"], caption="By: @Nexiou"
+                document=i["snapUrls"]["mediaUrl"], caption="By: @Musicspotf"
             )
     except KeyError:
         await update.reply_text(
             text="No Public Stories for past 24Hrs\n\n❌ OR INVALID USERNAME", quote=True
         )
 
-    await update.reply_text(text="Done ✨ @Nexiuo", quote=True)
+    await update.reply_text(text="Done ✨ @Musicspotf", quote=True)
